ba44d01371184c4e4d46470b79179931c81b1103
bgalgamu <kevin.si1396@gmail.com>
2023-05-16 10:41:51 +0200

    remove duplicate jsonProperty

--- web/src/main/java/it/pagopa/selfcare/party/registry_proxy/web/model/GeographicTaxonomyResource.java
+++ web/src/main/java/it/pagopa/selfcare/party/registry_proxy/web/model/GeographicTaxonomyResource.java
@@ -11,7 +11,6 @@ public class GeographicTaxonomyResource {
     private String geotaxId; //REQUIRED
     @JsonProperty("desc")
     @ApiModelProperty(value = "${swagger.geographicTaxonomy.model.description}")
-    @JsonProperty("desc")
     private String description;
     @ApiModelProperty(value = "${swagger.geographicTaxonomy.model.istatCode}")
     @JsonProperty("istat_code")//REQUIRED
