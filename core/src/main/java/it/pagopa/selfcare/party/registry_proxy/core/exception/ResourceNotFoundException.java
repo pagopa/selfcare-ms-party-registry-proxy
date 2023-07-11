6e6386020a906cfced3962e9203bb3131c6a6a20
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2023-07-11 12:14:23 +0200

    fix NR legal-address response

--- core/src/main/java/it/pagopa/selfcare/party/registry_proxy/core/exception/ResourceNotFoundException.java
+++ core/src/main/java/it/pagopa/selfcare/party/registry_proxy/core/exception/ResourceNotFoundException.java
@@ -1,4 +1,10 @@
 package it.pagopa.selfcare.party.registry_proxy.core.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
