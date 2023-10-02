278db2eaa6c95e4f60bebae6683893cbb8d64dea
Pierpaolo Di Dato <pierpaolo.didato@emeal.nttdata.com>
2023-10-02 11:07:12 +0200

    Rename pdnd resources

--- web/src/main/java/it/pagopa/selfcare/party/registry_proxy/web/model/mapper/StationMapper.java
+++ web/src/main/java/it/pagopa/selfcare/party/registry_proxy/web/model/mapper/StationMapper.java
@@ -1,11 +1,11 @@
 package it.pagopa.selfcare.party.registry_proxy.web.model.mapper;
 
 import it.pagopa.selfcare.party.registry_proxy.connector.model.Station;
-import it.pagopa.selfcare.party.registry_proxy.web.model.PDNDResource;
+import it.pagopa.selfcare.party.registry_proxy.web.model.StationResource;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface StationMapper {
-    PDNDResource toResource(Station pdnd);
+    StationResource toResource(Station pdnd);
 
 }
