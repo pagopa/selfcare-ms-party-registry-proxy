0207239e689dc34f64afdcaa2252a5a29b531415
Pierpaolo Di Dato <pierpaolo.didato@emeal.nttdata.com>
2023-10-10 15:54:26 +0200

    Removed validation constraints on resource objects

--- web/src/main/java/it/pagopa/selfcare/party/registry_proxy/web/model/StationResource.java
+++ web/src/main/java/it/pagopa/selfcare/party/registry_proxy/web/model/StationResource.java
@@ -1,43 +1,35 @@
 package it.pagopa.selfcare.party.registry_proxy.web.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import it.pagopa.selfcare.party.registry_proxy.connector.model.Origin;
 import it.pagopa.selfcare.party.registry_proxy.connector.model.Station;
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-
 @Data
 public class StationResource implements Station {
 
-    @ApiModelProperty(value = "${swagger.model.station.id}", required = true)
-    @JsonProperty(required = true)
+    @ApiModelProperty(value = "${swagger.model.station.id}")
     private String id;
 
-    @ApiModelProperty(value = "${swagger.model.station.originId}", required = true)
-    @JsonProperty(required = true)
+    @ApiModelProperty(value = "${swagger.model.station.originId}")
     private String originId;
 
-    private boolean anacEngaged;
-    
-    private boolean anacEnabled;
-
-    @ApiModelProperty(value = "${swagger.model.station.taxCode}", required = true)
-    @JsonProperty(required = true)
+    @ApiModelProperty(value = "${swagger.model.station.taxCode}")
     private String taxCode;
 
-    @ApiModelProperty(value = "${swagger.model.station.description}", required = true)
-    @JsonProperty(required = true)
+    @ApiModelProperty(value = "${swagger.model.station.description}")
     private String description;
 
-    @ApiModelProperty(value = "${swagger.model.station.digitalAddress}", required = true)
-    @JsonProperty(required = true)
-    @Email
+    @ApiModelProperty(value = "${swagger.model.station.digitalAddress}")
     private String digitalAddress;
 
-    @ApiModelProperty(value = "${swagger.model.*.origin}", required = true)
-    @JsonProperty(required = true)
+    @ApiModelProperty(value = "${swagger.model.station.anacEngaged}")
+    private boolean anacEngaged;
+
+    @ApiModelProperty(value = "${swagger.model.station.anacEnabled}")
+    private boolean anacEnabled;
+
+    @ApiModelProperty(value = "${swagger.model.*.origin}")
     private Origin origin;
 
 }
