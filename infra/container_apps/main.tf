e52b47605f1e5fbb3d92ffe6b9e3c86a3270d748
manuraf <manuel.rafeli@pagopa.it>
2024-03-29 11:47:12 +0100

    rename module and scale replicas dev

--- infra/container_apps/main.tf
+++ infra/container_apps/main.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
   features {}
 }
 
-module "container_app_dashboard_backend" {
+module "container_app_party_reg_proxy" {
   source = "github.com/pagopa/selfcare-commons//infra/terraform-modules/container_app_microservice?ref=main"
 
   is_pnpg = var.is_pnpg
@@ -17,9 +17,10 @@ module "container_app_dashboard_backend" {
   container_app      = var.container_app
   container_app_name = "party-reg-proxy"
   image_name         = "selfcare-ms-party-registry-proxy"
-  image_tag          = var.image_tag
+  image_tag          = "releases-1.0.0"
   app_settings       = var.app_settings
   secrets_names      = var.secrets_names
+  workload_profile_name = var. workload_profile_name
 
   tags = var.tags
 }
