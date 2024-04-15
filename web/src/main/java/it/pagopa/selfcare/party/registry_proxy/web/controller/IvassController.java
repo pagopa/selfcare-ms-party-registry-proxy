d2a6de56cb1ac3d020b6bede18160ec704101dcb
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2024-04-15 16:37:23 +0200

    Updates

--- web/src/main/java/it/pagopa/selfcare/party/registry_proxy/web/controller/IvassController.java
+++ web/src/main/java/it/pagopa/selfcare/party/registry_proxy/web/controller/IvassController.java
@@ -53,7 +53,7 @@ public class IvassController {
 
     @GetMapping("/origin/{originId}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "${swagger.api.insurance-company.search.byIvassCode.summary}", notes = "${swagger.api.insurance-company.search.byIvassCode.notes}")
+    @ApiOperation(value = "${swagger.api.insurance-company.search.byOriginId.summary}", notes = "${swagger.api.insurance-company.search.byOriginId.notes}")
     public InsuranceCompanyResource searchByOriginId(@ApiParam("${swagger.model.insurance-company.originId}")
                                                      @PathVariable("originId") String originId) {
         log.trace("searchByOriginId start");
