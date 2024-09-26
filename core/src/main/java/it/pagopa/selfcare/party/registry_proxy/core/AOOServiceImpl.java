d6c27babd3e122adb992c06b03a58c25d9963229
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2024-09-26 11:57:16 +0200

    feat: added scheduler for AOO IPA index

--- core/src/main/java/it/pagopa/selfcare/party/registry_proxy/core/AOOServiceImpl.java
+++ core/src/main/java/it/pagopa/selfcare/party/registry_proxy/core/AOOServiceImpl.java
@@ -1,12 +1,19 @@
 package it.pagopa.selfcare.party.registry_proxy.core;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import it.pagopa.selfcare.party.registry_proxy.connector.api.IndexSearchService;
+import it.pagopa.selfcare.party.registry_proxy.connector.api.IndexWriterService;
 import it.pagopa.selfcare.party.registry_proxy.connector.model.*;
 import it.pagopa.selfcare.party.registry_proxy.connector.exception.ResourceNotFoundException;
+import it.pagopa.selfcare.party.registry_proxy.connector.rest.client.OpenDataRestClient;
+import it.pagopa.selfcare.party.registry_proxy.connector.rest.model.IPAOpenDataAOO;
 import it.pagopa.selfcare.party.registry_proxy.core.exception.TooManyResourceFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,9 +24,13 @@ class AOOServiceImpl implements AOOService {
 
     private final IndexSearchService<AOO> indexSearchService;
     private final InstitutionService institutionService;
+    private final IndexWriterService<AOO> aooIndexWriterService;
+    private final OpenDataRestClient openDataRestClient;
 
     AOOServiceImpl(IndexSearchService<AOO> indexSearchService,
-                   InstitutionService institutionService) {
+                   InstitutionService institutionService, IndexWriterService<AOO> aooIndexWriterService, OpenDataRestClient openDataRestClient) {
+        this.aooIndexWriterService = aooIndexWriterService;
+        this.openDataRestClient = openDataRestClient;
         log.trace("Initializing {}", AOOServiceImpl.class.getSimpleName());
         this.indexSearchService = indexSearchService;
         this.institutionService = institutionService;
@@ -53,6 +64,36 @@ class AOOServiceImpl implements AOOService {
         }
         log.debug("find AOO by CodiceUniAOO result = {}", aoo);
         log.trace("find AOO by CodiceUniAOO end");
+        return aoo;
+    }
+
+    @Scheduled(cron = "0 0 2 * * *")
+    void updateAOOsIndex() {
+        log.trace("start update AOO IPA index");
+        List<AOO> aoo = getAOOs();
+        if (!aoo.isEmpty()) {
+            aooIndexWriterService.cleanIndex(Entity.AOO.toString());
+            aooIndexWriterService.adds(aoo);
+        }
+        log.trace("updated AOO IPA index end");
+    }
+
+    private List<AOO> getAOOs() {
+        log.trace("getAOOs start");
+        List<AOO> aoo;
+        final String csv = openDataRestClient.retrieveAOOs();
+
+        try (Reader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(csv.getBytes())))) {
+            CsvToBean<AOO> csvToBean = new CsvToBeanBuilder<AOO>(reader)
+                    .withType(IPAOpenDataAOO.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            aoo = csvToBean.parse();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        log.trace("getAOOs end");
         return aoo;
     }
 }
