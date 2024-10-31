7ea88674e3887d54b272ae784dbefcce3f6ef6de
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2024-10-31 09:39:44 +0100

    fix: removed retry tag

--- connector/rest/src/main/java/it/pagopa/selfcare/party/registry_proxy/connector/rest/PDNDInfoCamereConnectorImpl.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/party/registry_proxy/connector/rest/PDNDInfoCamereConnectorImpl.java
@@ -20,50 +20,61 @@ import java.util.List;
 @Slf4j
 @Service
 public class PDNDInfoCamereConnectorImpl implements PDNDInfoCamereConnector {
-    public static final String ERROR_PDND_INFO_CAMERE_REST_CLIENT_MESSAGE = "Error pdnd-infocamere rest client, message: %s";
+  public static final String ERROR_PDND_INFO_CAMERE_REST_CLIENT_MESSAGE =
+      "Error pdnd-infocamere rest client, message: %s";
 
-    private final PDNDInfoCamereRestClient pdndInfoCamereRestClient;
-    private final PDNDBusinessMapper pdndBusinessMapper;
-    private final TokenProvider tokenProvider;
-    private final PDNDInfoCamereRestClientConfig pdndInfoCamereRestClientConfig;
+  private final PDNDInfoCamereRestClient pdndInfoCamereRestClient;
+  private final PDNDBusinessMapper pdndBusinessMapper;
+  private final TokenProvider tokenProvider;
+  private final PDNDInfoCamereRestClientConfig pdndInfoCamereRestClientConfig;
 
-    public PDNDInfoCamereConnectorImpl(PDNDInfoCamereRestClient pdndInfoCamereRestClient, PDNDBusinessMapper pdndBusinessMapper, TokenProvider tokenProvider, PDNDInfoCamereRestClientConfig pdndInfoCamereRestClientConfig) {
-        this.pdndInfoCamereRestClient = pdndInfoCamereRestClient;
-        this.pdndBusinessMapper = pdndBusinessMapper;
-        this.tokenProvider = tokenProvider;
-        this.pdndInfoCamereRestClientConfig = pdndInfoCamereRestClientConfig;
-    }
+  public PDNDInfoCamereConnectorImpl(
+      PDNDInfoCamereRestClient pdndInfoCamereRestClient,
+      PDNDBusinessMapper pdndBusinessMapper,
+      TokenProvider tokenProvider,
+      PDNDInfoCamereRestClientConfig pdndInfoCamereRestClientConfig) {
+    this.pdndInfoCamereRestClient = pdndInfoCamereRestClient;
+    this.pdndBusinessMapper = pdndBusinessMapper;
+    this.tokenProvider = tokenProvider;
+    this.pdndInfoCamereRestClientConfig = pdndInfoCamereRestClientConfig;
+  }
 
-    @Override
-    @CircuitBreaker(name = "pdndInfoCamereCircuitbreaker", fallbackMethod = "fallbackRetrieveInstitutionByDescription")
-    @Retry(name = "retryServiceUnavailable")
-    public List<PDNDBusiness> retrieveInstitutionsPdndByDescription(String description) {
-        Assert.hasText(description, "Description is required");
-        ClientCredentialsResponse tokenResponse = tokenProvider.getTokenPdnd(pdndInfoCamereRestClientConfig.getPdndSecretValue());
-        String bearer = "Bearer " + tokenResponse.getAccessToken();
-        List<PDNDImpresa> result = pdndInfoCamereRestClient.retrieveInstitutionsPdndByDescription(description, bearer);
-        return pdndBusinessMapper.toPDNDBusinesses(result);
-    }
+  @Override
+  @CircuitBreaker(
+      name = "pdndInfoCamereCircuitbreaker",
+      fallbackMethod = "fallbackRetrieveInstitutionByDescription")
+  @Retry(name = "retryServiceUnavailable")
+  public List<PDNDBusiness> retrieveInstitutionsPdndByDescription(String description) {
+    Assert.hasText(description, "Description is required");
+    ClientCredentialsResponse tokenResponse =
+        tokenProvider.getTokenPdnd(pdndInfoCamereRestClientConfig.getPdndSecretValue());
+    String bearer = "Bearer " + tokenResponse.getAccessToken();
+    List<PDNDImpresa> result =
+        pdndInfoCamereRestClient.retrieveInstitutionsPdndByDescription(description, bearer);
+    return pdndBusinessMapper.toPDNDBusinesses(result);
+  }
 
-    public List<PDNDBusiness> fallbackRetrieveInstitutionByDescription(RuntimeException e) {
-        log.error(String.format(ERROR_PDND_INFO_CAMERE_REST_CLIENT_MESSAGE, e.getMessage()));
-        return List.of();
-    }
+  public List<PDNDBusiness> fallbackRetrieveInstitutionByDescription(RuntimeException e) {
+    log.error(String.format(ERROR_PDND_INFO_CAMERE_REST_CLIENT_MESSAGE, e.getMessage()));
+    return List.of();
+  }
 
-    @Override
-    //@CircuitBreaker(name = "pdndInfoCamereCircuitbreaker", fallbackMethod = "fallbackRetrieveInstitutionByTaxCode")
-    @Retry(name = "retryServiceUnavailable")
-    public PDNDBusiness retrieveInstitutionPdndByTaxCode(String taxCode) {
-        Assert.hasText(taxCode, "TaxCode is required");
-        ClientCredentialsResponse tokenResponse = tokenProvider.getTokenPdnd(pdndInfoCamereRestClientConfig.getPdndSecretValue());
-        String bearer = "Bearer " + tokenResponse.getAccessToken();
-        PDNDImpresa result = pdndInfoCamereRestClient.retrieveInstitutionPdndByTaxCode(taxCode, bearer).get(0);
-        return pdndBusinessMapper.toPDNDBusiness(result);
-    }
+  @Override
+  // @CircuitBreaker(name = "pdndInfoCamereCircuitbreaker", fallbackMethod =
+  // "fallbackRetrieveInstitutionByTaxCode")
+  // @Retry(name = "retryServiceUnavailable")
+  public PDNDBusiness retrieveInstitutionPdndByTaxCode(String taxCode) {
+    Assert.hasText(taxCode, "TaxCode is required");
+    ClientCredentialsResponse tokenResponse =
+        tokenProvider.getTokenPdnd(pdndInfoCamereRestClientConfig.getPdndSecretValue());
+    String bearer = "Bearer " + tokenResponse.getAccessToken();
+    PDNDImpresa result =
+        pdndInfoCamereRestClient.retrieveInstitutionPdndByTaxCode(taxCode, bearer).get(0);
+    return pdndBusinessMapper.toPDNDBusiness(result);
+  }
 
-    public PDNDBusiness fallbackRetrieveInstitutionByTaxCode(RuntimeException e) {
-        log.error(String.format(ERROR_PDND_INFO_CAMERE_REST_CLIENT_MESSAGE, e.getMessage()), e);
-        throw new ResourceNotFoundException("");
-    }
-
+  public PDNDBusiness fallbackRetrieveInstitutionByTaxCode(RuntimeException e) {
+    log.error(String.format(ERROR_PDND_INFO_CAMERE_REST_CLIENT_MESSAGE, e.getMessage()), e);
+    throw new ResourceNotFoundException("");
+  }
 }
