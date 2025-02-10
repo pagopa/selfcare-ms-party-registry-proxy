6ab1b8022c495d1c695eb6c5168edd21530bc6da
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2025-02-10 15:34:50 +0100

    Added swagger configuration

--- app/src/test/java/it/pagopa/selfcare/party/registry_proxy/web/config/SwaggerConfigTest.java
+++ app/src/test/java/it/pagopa/selfcare/party/registry_proxy/web/config/SwaggerConfigTest.java
@@ -14,7 +14,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -27,7 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
         SwaggerConfig.class,
         WebConfig.class,
 })
-@EnableOpenApi
 @EnableWebMvc
 @ComponentScan(basePackages = {"it.pagopa.selfcare.party.registry_proxy.web.controller", "it.pagopa.selfcare.party.registry_proxy.web.model.mapper"})
 @TestPropertySource(locations = "classpath:config/application.yml")
@@ -66,7 +64,7 @@ class SwaggerConfigTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    /*@Test
+    @Test
     void swaggerSpringPlugin() throws Exception {
         MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
         mockMvc.perform(MockMvcRequestBuilders.get("/v3/api-docs").accept(MediaType.APPLICATION_JSON))
@@ -82,5 +80,5 @@ class SwaggerConfigTest {
                     Files.createDirectories(basePath);
                     Files.write(basePath.resolve("api-docs.json"), formatted.getBytes());
                 });
-    }*/
+    }
 }
