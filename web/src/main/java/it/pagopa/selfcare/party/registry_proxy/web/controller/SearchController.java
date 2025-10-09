1179f5965d1e308c2163e9648e7cd0b1acffad1c
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2025-10-09 09:12:43 +0200

    [SELC-7816] chore: Update openApi (#283)

--- web/src/main/java/it/pagopa/selfcare/party/registry_proxy/web/controller/SearchController.java
+++ web/src/main/java/it/pagopa/selfcare/party/registry_proxy/web/controller/SearchController.java
@@ -1,6 +1,10 @@
 package it.pagopa.selfcare.party.registry_proxy.web.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import it.pagopa.selfcare.party.registry_proxy.connector.model.SearchServiceInstitution;
 import it.pagopa.selfcare.party.registry_proxy.core.SearchService;
 import lombok.extern.slf4j.Slf4j;
@@ -26,14 +30,25 @@ public class SearchController {
     this.searchService = searchService;
   }
 
+  @Tag(name = "external-v2")
   @GetMapping("/institutions")
   @PreAuthorize("hasPermission(new it.pagopa.selfcare.party.registry_proxy.web.security.FilterAuthorityDomain('PAGOPA'), 'Selc:SearchInstitutions')")
+  @Operation(summary = "${swagger.api.search.institutions.summary}",
+    description = "${swagger.api.search.institutions.notes}",
+    operationId = "retrieveInstitutionOnSearchEngine"
+   )
   public ResponseEntity<List<SearchServiceInstitution>> searchInstitutions(
+    @Parameter(description = "${swagger.model.*.products}")
     @RequestParam(required = false) List<String> products,
+    @Parameter(description = "${swagger.model.*.institution.types}")
     @RequestParam(required = false) List<String> institutionTypes,
+    @Parameter(description = "${swagger.model.institution.taxCode}")
     @RequestParam(required = false) String taxCode,
+    @Parameter(description = "${swagger.model.*.searchText}")
     @RequestParam(defaultValue = "*") String searchText,
+    @Parameter(description = "${swagger.model.*.limit}")
     @RequestParam(defaultValue = "50") int top,
+    @Parameter(description = "${swagger.model.*.page}")
     @RequestParam(defaultValue = "0") int skip) {
 
     try {
