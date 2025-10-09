5423130664d5bc9842edcd896cba2068f4fa781d
pierpaolo.didato@emeal.nttdata.com <pierpaolo.didato@emeal.nttdata.com>
2025-10-09 11:48:16 +0200

    Updates

--- infra/container_apps/data.tf
+++ infra/container_apps/data.tf
@@ -1,9 +1,9 @@
-data "azurerm_container_app" "ca" {
-  name                = var.ca_name
-  resource_group_name = var.ca_rg_name
-}
-
 data "azurerm_container_app_environment" "cae" {
   name                = var.cae_name
   resource_group_name = var.cae_rg_name
+}
+
+data "azurerm_container_app" "ca" {
+  name                = var.ca_name
+  resource_group_name = var.ca_rg_name
 }
