75a6dbe2551e19517422a2d7914ecf22c768927c
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2025-10-09 14:35:45 +0200

    [SELC-7826] feat: Fixed search by category when category major than 3 characters

--- connector/lucene/src/main/java/it/pagopa/selfcare/party/registry_proxy/connector/lucene/reader/IndexSearchServiceTemplate.java
+++ connector/lucene/src/main/java/it/pagopa/selfcare/party/registry_proxy/connector/lucene/reader/IndexSearchServiceTemplate.java
@@ -14,9 +14,9 @@ import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
+import org.owasp.encoder.Encode;
 import org.springframework.util.Assert;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
@@ -26,13 +26,16 @@ abstract class IndexSearchServiceTemplate<T> implements IndexSearchService<T> {
 
     private static final String FIELD_IS_REQUIRED = "A search field is required";
     private static final String VALUE_IS_REQUIRED = "A search value is required";
+    private static final String FULL_TEXT_SEARCH_START = "fullTextSearch start";
+    private static final String FULL_TEXT_SEARCH_RESULT = "fullTextSearch result = {}";
+    private static final String FULL_TEXT_SEARCH_END = "fullTextSearch end";
 
     private final DirectoryReaderFactory directoryReaderFactory;
     private final Analyzer analyzer;
     private final Function<Document, T> documentConverter;
 
 
-    public IndexSearchServiceTemplate(Directory directory, Analyzer analyzer, Function<Document, T> documentConverter) {
+    protected IndexSearchServiceTemplate(Directory directory, Analyzer analyzer, Function<Document, T> documentConverter) {
         log.trace("Initializing {}", getClass().getSimpleName());
         directoryReaderFactory = new DirectoryReaderFactory(directory);
         this.analyzer = analyzer;
@@ -43,7 +46,7 @@ abstract class IndexSearchServiceTemplate<T> implements IndexSearchService<T> {
     @SneakyThrows
     @Override
     public QueryResult<T> fullTextSearch(SearchField field, String value, int page, int limit) {
-        log.trace("fullTextSearch start");
+        log.trace(FULL_TEXT_SEARCH_START);
         log.debug("fullTextSearch field = {}, value = {}, page = {}, limit = {}", field, value, page, limit);
         Assert.notNull(field, FIELD_IS_REQUIRED);
         Assert.notNull(value, VALUE_IS_REQUIRED);
@@ -63,52 +66,75 @@ abstract class IndexSearchServiceTemplate<T> implements IndexSearchService<T> {
         }
 
         final QueryResult<T> queryResult = getQueryResult(categories, hits.totalHits.value);
-        log.debug("fullTextSearch result = {}", queryResult);
-        log.trace("fullTextSearch end");
+        log.debug(FULL_TEXT_SEARCH_RESULT, queryResult);
+        log.trace(FULL_TEXT_SEARCH_END);
         return queryResult;
     }
 
     @SneakyThrows
     @Override
-    public QueryResult<T> fullTextSearch(SearchField field, String value, SearchField filter, String categories, int page, int limit) {
-        log.trace("fullTextSearch start");
-        log.debug("fullTextSearch field = {}, value = {}, filter = {}, categories = {}, page = {}, limit = {}", field, value, filter, categories, page, limit);
-        Assert.notNull(field, FIELD_IS_REQUIRED);
-        Assert.notNull(value, VALUE_IS_REQUIRED);
-        Assert.notNull(filter, FIELD_IS_REQUIRED);
-        Assert.notNull(categories, VALUE_IS_REQUIRED);
-        Assert.isTrue(page > 0, "A page number must be great than 0");
-        Assert.isTrue(limit > 0, "A limit result must be great than or equal to 1");
+    public QueryResult<T> fullTextSearch(SearchField searchFieldDescription,
+                                         String currentDescription,
+                                         SearchField searchFieldCategory,
+                                         String currentCategories,
+                                         int page,
+                                         int limit) {
+        log.trace(FULL_TEXT_SEARCH_START);
+
+        log.debug("fullTextSearch field = {}, value = {}, filter = {}, categories = {}, page = {}, limit = {}",
+                searchFieldDescription, Encode.forJava(currentDescription), searchFieldCategory, Encode.forJava(currentCategories), page, limit);
+
+        Assert.notNull(searchFieldDescription, FIELD_IS_REQUIRED);
+        Assert.notNull(currentDescription, VALUE_IS_REQUIRED);
+        Assert.notNull(searchFieldCategory, FIELD_IS_REQUIRED);
+        Assert.notNull(currentCategories, VALUE_IS_REQUIRED);
+        Assert.isTrue(page > 0, "A page number must be greater than 0");
+        Assert.isTrue(limit > 0, "A limit result must be greater than or equal to 1");
+
         final DirectoryReader reader = directoryReaderFactory.create();
         final IndexSearcher indexSearcher = new IndexSearcher(reader);
-        final TopScoreDocCollector collector = TopScoreDocCollector.create(reader.numDocs(), Integer.MAX_VALUE);
-        final QueryParser parser = new QueryParser(field.toString(), analyzer);
-        final QueryParser cparser = new QueryParser(filter.toString(), analyzer);
-        parser.setPhraseSlop(4);
-        Query allDcs = new MatchAllDocsQuery();
-        BooleanQuery.Builder bq = new BooleanQuery.Builder();
-        Query query = parser.parse(value);
 
-        bq.add(allDcs, BooleanClause.Occur.SHOULD);
-        bq.add(cparser.parse(categories.replace(",", " OR ")), BooleanClause.Occur.MUST);
-        bq.add(query, BooleanClause.Occur.SHOULD);
-        try {
-            indexSearcher.search(parser.parse(bq.build().toString()), collector);
-        } catch (Exception e) {
-            log.error(e.getMessage());
+        final TopScoreDocCollector collector =
+                TopScoreDocCollector.create(reader.numDocs(), Integer.MAX_VALUE);
+
+        QueryParser descriptionParser = new QueryParser(searchFieldDescription.toString(), analyzer);
+        descriptionParser.setPhraseSlop(4);
+        Query descriptionQuery = descriptionParser.parse(currentDescription);
+
+        BooleanQuery.Builder categoryQueryBuilder = new BooleanQuery.Builder();
+
+        for (String category : currentCategories.split(",")) {
+            String trimmedCategory = category.trim();
+            if (!trimmedCategory.isEmpty()) {
+                Query categoryQuery = new TermQuery(new Term(searchFieldCategory.toString(), trimmedCategory));
+                categoryQueryBuilder.add(categoryQuery, BooleanClause.Occur.SHOULD);
+            }
         }
+
+        Query categoriesQuery = categoryQueryBuilder.build();
+        Query boostedDescriptionQuery = new BoostQuery(descriptionQuery, 5.0f);
+
+        BooleanQuery.Builder finalQueryBuilder = new BooleanQuery.Builder();
+        finalQueryBuilder.add(categoriesQuery, BooleanClause.Occur.MUST);
+        finalQueryBuilder.add(boostedDescriptionQuery, BooleanClause.Occur.SHOULD);
+
+        Query finalQuery = finalQueryBuilder.build();
+
+        indexSearcher.search(finalQuery, collector);
 
         final TopDocs hits = collector.topDocs((page - 1) * limit, limit);
 
-        final List<T> foundCategories = new ArrayList<>(hits.scoreDocs.length);
-        for (ScoreDoc scoreDoc : hits.scoreDocs) {
-            foundCategories.add(documentConverter.apply(indexSearcher.doc(scoreDoc.doc)));
-        }
-        final QueryResult<T> queryResult = getQueryResult(foundCategories, hits.totalHits.value);
-        log.debug("fullTextSearch result = {}", queryResult);
-        log.trace("fullTextSearch end");
-        return queryResult;
+        final List<T> results = new ArrayList<>(hits.scoreDocs.length);
 
+        for (ScoreDoc scoreDoc : hits.scoreDocs) {
+            results.add(documentConverter.apply(indexSearcher.doc(scoreDoc.doc)));
+        }
+
+        final QueryResult<T> queryResult = getQueryResult(results, hits.totalHits.value);
+        log.debug(FULL_TEXT_SEARCH_RESULT, queryResult);
+        log.trace(FULL_TEXT_SEARCH_END);
+
+        return queryResult;
     }
 
 
