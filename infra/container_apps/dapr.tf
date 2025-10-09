5024da4cf23112c55cd5d2a2a32fb5ef779c34e2
pierpaolo.didato@emeal.nttdata.com <pierpaolo.didato@emeal.nttdata.com>
2025-10-09 14:12:24 +0200

    Updates

--- infra/container_apps/dapr.tf
+++ infra/container_apps/dapr.tf
@@ -40,5 +40,5 @@ resource "azurerm_container_app_environment_dapr_component" "blob_state" {
     value = azurerm_storage_container.dapr_container.name
   }
 
-  scopes = [data.azurerm_container_app.ca.name]
+  # sscopes = [data.azurerm_container_app.ca.name]
 }
