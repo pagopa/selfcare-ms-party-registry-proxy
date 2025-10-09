a1299789039dbcbf946ac260fe543725accda923
pierpaolo.didato@emeal.nttdata.com <pierpaolo.didato@emeal.nttdata.com>
2025-10-09 18:15:33 +0200

    Updates

--- infra/container_apps/data.tf
+++ infra/container_apps/data.tf
@@ -3,11 +3,6 @@ data "azurerm_container_app_environment" "cae" {
   resource_group_name = var.cae_rg_name
 }
 
-data "azurerm_container_app" "ca" {
-  name                = var.ca_name
-  resource_group_name = var.ca_rg_name
-}
-
 data "azurerm_storage_account" "existing_logs_storage" {
   name                = "selcdstlogs"
   resource_group_name = var.existing_logs_rg
@@ -17,7 +12,6 @@ data "azurerm_key_vault" "key_vault" {
   resource_group_name = var.key_vault_resource_group_name
   name                = var.key_vault_name
 }
-
 
 data "azurerm_key_vault_secret" "logs_storage_access_key" {
   name         = "logs-storage-access-key"
