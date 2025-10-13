61a68efba64f2024707862387df32e325ed5de83
pierpaolo.didato@emeal.nttdata.com <pierpaolo.didato@emeal.nttdata.com>
2025-10-13 11:53:04 +0200

    Updates

--- infra/container_apps/dapr.tf
+++ infra/container_apps/dapr.tf
@@ -6,20 +6,21 @@ resource "azurerm_storage_container" "visura" {
 }
 
 resource "azurerm_container_app_environment_dapr_component" "blob_state" {
-  count                        = var.is_pnpg  ? 0 : 1
+  count = var.is_pnpg ? 0 : 1
+
   name                         = "blobstorage-state"
-  container_app_environment_id = data.azurerm_container_app_environment.cae.id
+  container_app_environment_id = local.cae_id
   component_type               = "state.azure.blobstorage"
   version                      = "v1"
 
   metadata {
     name  = "accountName"
-    value = data.azurerm_storage_account.existing_logs_storage.name
+    value = local.storage_account_name
   }
 
   metadata {
     name  = "containerName"
-    value = azurerm_storage_container.visura.name
+    value = try(azurerm_storage_container.visura[0].name, null)
   }
 
   metadata {
@@ -27,9 +28,5 @@ resource "azurerm_container_app_environment_dapr_component" "blob_state" {
     value = data.azurerm_user_assigned_identity.cae_identity.client_id
   }
 
-  scopes = [data.azurerm_container_app.ca.dapr[0].app_id]
-
-  lifecycle {
-    prevent_destroy = false
-  }
+  scopes = [local.container_app_id]
 }
--- infra/container_apps/data.tf
+++ infra/container_apps/data.tf
@@ -1,5 +1,5 @@
 data "azurerm_container_app_environment" "cae" {
-  count               = var.is_pnpg == true ? 0 : 1
+  count               = var.is_pnpg ? 0 : 1
   name                = local.container_app_environment_name
   resource_group_name = var.cae_rg_name
 }
@@ -18,12 +18,21 @@ data "azurerm_storage_account" "existing_logs_storage" {
 
 data "azurerm_key_vault" "key_vault" {
   count               = var.is_pnpg ? 0 : 1
-  resource_group_name = var.key_vault_resource_group_name
   name                = var.key_vault_name
+  resource_group_name = var.key_vault_resource_group_name
 }
 
 data "azurerm_key_vault_secret" "logs_storage_access_key" {
-  count      = var.is_pnpg ? 0 : 1
-  name         = "logs-storage-access-key"
+  count       = var.is_pnpg ? 0 : 1
+  name        = "logs-storage-access-key"
   key_vault_id = try(data.azurerm_key_vault.key_vault[0].id, null)
+}
+
+locals {
+  cae_id               = try(data.azurerm_container_app_environment.cae[0].id, null)
+  container_app_id     = try(data.azurerm_container_app.ca[0].id, null)
+  storage_account_id   = try(data.azurerm_storage_account.existing_logs_storage[0].id, null)
+  storage_account_name = try(data.azurerm_storage_account.existing_logs_storage[0].name, null)
+  key_vault_id         = try(data.azurerm_key_vault.key_vault[0].id, null)
+  logs_storage_key     = try(data.azurerm_key_vault_secret.logs_storage_access_key[0].value, null)
 }
