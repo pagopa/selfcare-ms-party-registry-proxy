a227fb96031f4fd40979a67de43de9009b899b4d
pierpaolo.didato@emeal.nttdata.com <pierpaolo.didato@emeal.nttdata.com>
2025-10-13 10:35:44 +0200

    Updates

--- infra/container_apps/variables.tf
+++ infra/container_apps/variables.tf
@@ -142,7 +142,7 @@ variable "cae_rg_name" {
 variable "storage_logs" {
   type        = string
   description = "Storage audit logs name"
-  default     = "storage-name"
+  default     = "storagename"
 }
 
 
