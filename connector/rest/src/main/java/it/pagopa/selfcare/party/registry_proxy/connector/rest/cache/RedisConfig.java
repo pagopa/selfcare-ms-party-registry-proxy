48f9d0fabeff9bc8f3ab243abf8b57b521777a44
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2025-10-25 09:55:28 +0200

    Fixed serialization

--- connector/rest/src/main/java/it/pagopa/selfcare/party/registry_proxy/connector/rest/cache/RedisConfig.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/party/registry_proxy/connector/rest/cache/RedisConfig.java
@@ -1,11 +1,5 @@
 package it.pagopa.selfcare.party.registry_proxy.connector.rest.cache;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +12,6 @@ import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -54,24 +47,11 @@ public class RedisConfig {
 
     @Bean(name = "redisCacheManager")
     public RedisCacheManager redisCacheManager(JedisConnectionFactory jedisConnectionFactory) {
-
-        ObjectMapper om = new ObjectMapper();
-        om.registerModule(new JavaTimeModule());
-        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.activateDefaultTyping(
-                BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build(),
-                ObjectMapper.DefaultTyping.NON_FINAL
-        );
-
-        GenericJackson2JsonRedisSerializer json = new GenericJackson2JsonRedisSerializer(om);
-        StringRedisSerializer string = new StringRedisSerializer();
-
         RedisCacheConfiguration cfg = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofHours(1))
                 .disableCachingNullValues()
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(string))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(json));
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
 
         return RedisCacheManager.builder(RedisCacheWriter.nonLockingRedisCacheWriter(jedisConnectionFactory))
                 .cacheDefaults(cfg)
@@ -79,5 +59,7 @@ public class RedisConfig {
                 .disableCreateOnMissingCache()
                 .build();
     }
+
+
 
 }
