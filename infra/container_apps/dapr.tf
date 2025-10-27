379f96bf3a647b2f62a1ecd2627a2a0add075ec1
pierpaolo.didato@emeal.nttdata.com <pierpaolo.didato@emeal.nttdata.com>
2025-10-27 09:59:55 +0100

    Updates

--- infra/container_apps/dapr.tf
+++ infra/container_apps/dapr.tf
@@ -5,6 +5,7 @@ resource "azurerm_storage_container" "visura" {
   container_access_type = "private"
 }
 
+# State store blob per non-PNPG (esistente)
 resource "azurerm_container_app_environment_dapr_component" "blob_state" {
   count = var.is_pnpg ? 0 : 1
 
@@ -38,4 +39,22 @@ resource "azurerm_container_app_environment_dapr_component" "blob_state" {
   lifecycle {
     prevent_destroy = false
   }
+}
+
+# State store in-memory per PNPG
+resource "azurerm_container_app_environment_dapr_component" "memory_state" {
+  count = var.is_pnpg ? 1 : 0
+
+  name                         = "blobstorage-state"  # Stesso nome!
+  container_app_environment_id = local.cae_id
+  component_type               = "state.in-memory"
+  version                      = "v1"
+
+  metadata {
+    name  = "actorStateStore"
+    value = "true"
+  }
+
+  # Nota: non mettere scopes se non hai il data source disponibile quando is_pnpg = true
+  # scopes = []  # Lascia vuoto per applicare a tutte le app nell'environment
 }
