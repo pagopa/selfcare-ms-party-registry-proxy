8120fe90102aa5a27390dd162121be5b3cdbb301
pierpaolo.didato@emeal.nttdata.com <pierpaolo.didato@emeal.nttdata.com>
2025-10-27 10:38:16 +0100

    Updates

--- infra/container_apps/dapr.tf
+++ infra/container_apps/dapr.tf
@@ -5,14 +5,13 @@ resource "azurerm_storage_container" "visura" {
   container_access_type = "private"
 }
 
-# State store blob per non-PNPG (esistente)
 resource "azurerm_container_app_environment_dapr_component" "blob_state" {
   count = var.is_pnpg ? 0 : 1
 
   name                         = "blobstorage-state"
   container_app_environment_id = local.cae_id
   component_type               = "state.azure.blobstorage"
-  version                      = "v1"
+  version                      = "v2"
 
   metadata {
     name  = "accountName"
@@ -25,13 +24,13 @@ resource "azurerm_container_app_environment_dapr_component" "blob_state" {
   }
 
   metadata {
-    name  = "actorStateStore"
-    value = "true"
+    name  = "azureClientId"
+    value = data.azurerm_user_assigned_identity.cae_identity.client_id
   }
 
   metadata {
-    name  = "azureClientId"
-    value = data.azurerm_user_assigned_identity.cae_identity.client_id
+    name  = "actorStateStore"
+    value = "true"
   }
 
   scopes = [data.azurerm_container_app.ca[0].dapr[0].app_id]
@@ -39,22 +38,4 @@ resource "azurerm_container_app_environment_dapr_component" "blob_state" {
   lifecycle {
     prevent_destroy = false
   }
-}
-
-# State store in-memory per PNPG
-resource "azurerm_container_app_environment_dapr_component" "memory_state" {
-  count = var.is_pnpg ? 1 : 0
-
-  name                         = "blobstorage-state"  # Stesso nome!
-  container_app_environment_id = "selc-d-pnpg-cae-cp"
-  component_type               = "state.in-memory"
-  version                      = "v1"
-
-  metadata {
-    name  = "actorStateStore"
-    value = "true"
-  }
-
-  # Nota: non mettere scopes se non hai il data source disponibile quando is_pnpg = true
-  # scopes = []  # Lascia vuoto per applicare a tutte le app nell'environment
 }
